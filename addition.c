78cef44d076333eb72f612ba0b6d8d911e9b6957
rohitmaity001 <110299981+rohitmaity001@users.noreply.github.com>
2022-07-30 21:45:07 +0530

    Add files via upload
    it's a clanuage code about addition of two number

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter a number\t");
+	scanf("%d",&a);
+	printf("enter second number\t");
+		scanf("%d",&b);
+	c=a+b;
+	printf("your answer is \t%d",c);
+	return 0;
+}
