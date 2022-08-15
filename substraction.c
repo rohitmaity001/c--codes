8ed5612ec90619f830ed68e6af027a74b80b8ba2
rohit maity <110299981+rohitmaity001@users.noreply.github.com>
2022-08-15 21:26:13 +0530

    Add files via upload

---
+++ substraction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int a,b,c;
+int main()
+{
+	printf("Enter your first number \t");
+	scanf("%d",&a);
+	printf("Enter your second number \t");
+	scanf("%d",&b);
+	c=a-b;
+	printf("your answer is %d \t",c);
+	return 0;
+}
